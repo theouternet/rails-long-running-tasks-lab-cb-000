ffd98999862c4ea0ae333a91ccc5425c3123a035
Jeremy R <jeremy.rougeau@aletheiainteractive.com>
2020-06-01 05:11:17 +0000

    Done.

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,14 @@
 class SongsController < ApplicationController
-
+   require 'csv' 
   def index
     @songs = Song.all
+  end
+
+ def upload
+    CSV.foreach(params["file"].path, headers: true) do |song|
+      Song.create(title: song[0], artist_name: song[1])
+    end
+    redirect_to songs_path
   end
 
   def show
